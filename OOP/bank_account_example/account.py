0df218d73357e1eda727321e081f1a45a02f9858
Igor Melo <igorjmelo4@gmail.com>
2018-06-27 22:46:57 -0300

    Creating a Bank Account Object Example

---
+++ OOP/bank_account_example/account.py
@@ -0,0 +1,20 @@
+# Creating a Bank Account Object
+
+class Account:
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        with open(filepath, 'r') as file:
+            self.balance = int(file.read())
+
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+    def commit(self):
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
+
+account = Account("balance.txt")
